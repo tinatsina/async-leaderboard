f766539695a1cb26b4df0065e6398003bf46ab32
tinayetsina <tinayetsina@outlook.com>
2022-08-23 23:29:11 +0800

    update - push aync/await complete

--- src/modules/listLoader.js
+++ src/modules/listLoader.js
@@ -1,22 +1,17 @@
-function fetchLeaderBoard(list) {
-  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/fjsAKla7v4whxFIG6uwc/scores/', {
-    method: 'GET',
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  })
-    .then((response) => response.json())
-    .then((json) => {
-      json.result.forEach((item) => {
-        list.innerHTML += `
-              <li>
-                <p class="list-name">${item.user}</p>
-                <p class="list-score">${item.score}</p>
-              </li>
-        `;
-      });
-    });
-}
+const fetchLeaderBoard = async (list) => {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/fjsAKla7v4whxFIG6uwc/scores/');
+  const json = await response.json();
+  const listArray = json.result;
+
+  listArray.forEach((item) => {
+    list.innerHTML += `
+          <li>
+            <p class="list-name">${item.user}</p>
+            <p class="list-score">${item.score}</p>
+          </li>
+    `;
+  });
+};
 
 export default function listLoader() {
   document.addEventListener('DOMContentLoaded', () => {
